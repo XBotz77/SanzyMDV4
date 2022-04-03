8a96e45ad12adc8d24f0515e77c4d47ab6f480cb
XBotz77 <102786743+XBotz77@users.noreply.github.com>
2022-04-03 17:10:43 +0800

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create Saleh Botzz.
+   * Contact Me on wa.me/6285822558496
+   * Follow https://github.com/XBotz77
 */
 
 require('./config')
